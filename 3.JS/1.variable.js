c03727f14d4d0d76936a42226a380199b289702b
Shivam Singh <shivam>
2026-06-16 15:31:41 +0530

    variables

---
+++ 3.JS/1.variable.js
@@ -0,0 +1,32 @@
+// // 1.1: var - keyword, message- variable name, data - hey there (initialized value)
+// var message = "hey there";
+// var numberValue = 1231;
+// var valuefloat = 1.21;
+
+// console.log(message);
+// console.log(numberValue);
+// console.log(valuefloat);
+
+// 2:
+
+// // 2.1: var: re-decalration, assignment possible
+// var message = "This is a var varaible";
+
+// console.log(message);
+// var message = "This is a re-declared var variable";
+// console.log(message);
+// var message = "This is a final update";
+// console.log(message);
+
+// // 2.2: let : re-declaration not possible, but we allow assignment
+// let a = 20;
+// // let a = 20
+// console.log(a);
+// a = 30;
+// console.log(a);
+
+
+// 2.3: const: re-decalarion and assignment both are not allowed
+const a = 30;
+// const a = 40;
+// a = 50;
