c0b17f82d233cc21adc6dbac50c602d7dcb33580
Shivam Singh <shivam>
2026-06-17 14:46:40 +0530

    loops

---
+++ 3.JS/5.loops.js
@@ -0,0 +1,32 @@
+// // 1. for loop:  for(intilExpression, condition, updatedExpression)
+
+// // for (let i = 1; i <= 100; i++) {
+// //   console.log("Hi");
+// // }
+
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// const product = ["Laptop", "phone", "watch"];
+// // console.log(product[0]);
+// // console.log(product[1]);
+// // console.log(product[2]);
+// for (let i = 0; i < product.length; i++) {
+//   console.log(product[i]);
+// }
+
+// // 2. while loops:
+// let a = 1;
+
+// while (a <= 5) {
+//   console.log(a);
+//   a++;
+// }
+
+// 3. do..while loop: at least once, if the condition is false then also
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
