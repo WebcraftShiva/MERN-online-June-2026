c24924b3bc02274579f1826764f05e8561aed1d1
Shivam Singh <shivam>
2026-06-17 14:11:05 +0530

    conditional statements

---
+++ 3.JS/4.conditional_statement.js
@@ -0,0 +1,73 @@
+// let age =4;
+// let newAge = 20;
+// // 18>=: vote
+
+// // 1. if:
+// if (newAge >= age) {
+//   console.log("You can vote");
+// }
+
+// // 2. if..else:
+
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("Eligible for vote");
+// } else {
+//   console.log("Not eligible to vote");
+// }
+
+// // 3. if..else if..else: multiple confition
+
+// let mark = prompt("Enter your marks20");
+
+// if (mark >= 90) {
+//   console.log("A Grade");
+// } else if (mark >= 75) {
+//   console.log("B Grade");
+// } else if (mark >= 50) {
+//   console.log("C Grade");
+// } else {
+//   console.log("Failed");
+// }
+
+// // 4. Nested if:
+// let age = 15; // 18 >=  -> licence
+// let driveLicience = true;
+
+// if (age >= 18) {
+//   if (driveLicience) {
+//     console.log("Have a licence");
+//   } else {
+//     console.log("You need to get licence 1st");
+//   }
+// } else {
+//   console.log("You are a baby. Get old enough");
+// }
+
+// 5. switch:
+let day = 4;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+}
