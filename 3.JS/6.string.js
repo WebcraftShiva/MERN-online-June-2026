bcf3ea43db569b126e9ede096c67a440380c30e8
Shivam Singh <shivam>
2026-06-17 15:47:00 +0530

    string and its methods

---
+++ 3.JS/6.string.js
@@ -0,0 +1,54 @@
+// String: a sequence of characters inclosed in quotes-  " " , ' '
+// ex: "hello"  or 'hello'
+
+// let msg = prompt("Enter your name");
+
+// // 1. Im-mutable:
+// msg[6] = "x";
+// console.log(msg[6]);
+
+// // 2: trim()
+// // console.log(msg);
+// console.log(msg.trim());
+
+// // 3. toUppercase
+// let uppercase = msg.toUpperCase();
+// console.log(uppercase);
+
+// // 4. toLowerCase:
+// let lowerCase = msg.toLowerCase();
+// console.log(lowerCase);
+
+// // 5. indexof:
+// let textMsg = "Sonam_bewafa_hai";
+// console.log(textMsg.indexOf("bewafa"));
+// console.log(textMsg.indexOf("_"));
+
+// // 6. method chaining:
+// let fruitName = prompt();
+// // let fruitTrim = fruitName.trim();
+
+// // // console.log(fruitTrim);
+// // let fruitLowerCase = fruitTrim.toLowerCase();
+// // console.log(fruitLowerCase);
+// fruitName.trim().toLowerCase();
+// console.log(fruitName);
+
+// // 6. slice: (n,n-1) -> n= start, n-1 :end
+
+// let mrngMsg = "kaalu kaliya";
+
+// console.log(mrngMsg.slice(3));
+// console.log(mrngMsg.slice(-3));
+// console.log(mrngMsg.slice(3, 10));
+
+// // 7. replace:
+// let txt = "I love moon";
+// console.log(txt);
+// let newText = txt.replace("moon", "Yui");
+// console.log(newText);
+// console.log(txt);
+
+// 8. repeat:
+let message = "Mai tumhre liye chand taare le aau\n";
+console.log(message.repeat(3));
