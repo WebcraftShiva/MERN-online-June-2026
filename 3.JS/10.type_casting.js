dbdfac7831d40d3aef7c56e80f4ce55844798f99
Shivam Singh <shivam>
2026-06-18 16:05:44 +0530

    type casting

---
+++ 3.JS/10.type_casting.js
@@ -0,0 +1,24 @@
+// console.log(1 + 2);
+// console.log(typeof (1 + 2));
+// console.log(1 + "2"); // 1 (number) will be converted into string bcz + is a concat
+// console.log(typeof (1 + "2"));
+
+// console.log(10 - 2);
+// console.log(10 - "2"); // string value (2) will be changing to number before subtraction is done
+// console.log(typeof (10 - "2"));
+
+// console.log(10 + "Z");
+// console.log(10 - "Z");
+
+// console.log(10 + "2" + "A"); // 102A
+// console.log(10 + "2" - "A"); // NaN
+
+// string to Number
+
+let a = "20";
+// console.log(typeof a);
+// console.log(parseInt(a));
+// console.log(typeof parseInt(a));
+
+let b = Number(a);
+console.log(typeof b);
