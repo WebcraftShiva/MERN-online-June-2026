231ed1b49ba87f50c69405ed6bf0295e79136388
Shivam Singh <shivam>
2026-06-19 12:03:21 +0530

    functions

---
+++ 3.JS/11.function.js
@@ -0,0 +1,52 @@
+// // 1.
+// // function sum() { // function decalaration
+// //   let a = 20;
+// //   let b = 30;
+// //   return a + b;
+// // }
+
+// // sum(); // function call
+
+// // fn(): greet()
+
+// function greet(name1, name2) {
+//   // name is an argument of function
+//   // function body: excutable part of function
+//   console.log("Hello " + name1 + " " + name2);
+// }
+// greet("shivam", "Naina"); // parameter
+// // greet();
+
+// 2.
+
+// function multiply(a, b) {
+//   let c = a * b;
+//   console.log(c);
+//   console.log("hi");
+//   return;
+//   console.log("hi");
+//   console.log("hi");
+//   console.log("hi");
+//   console.log("hi");
+// }
+// multiply(2, 3);
+
+// 3. Anonymous function: function without name
+
+// function () {
+//   console.log("hello there!");
+// }
+
+// // 4. Arrow function:
+
+// const greet = () => {
+//   console.log("Hello there");
+// };
+
+// greet();
+
+// const arrow = () => {
+//   console.log("Hello i am fine");
+// };
+
+// arrow();
